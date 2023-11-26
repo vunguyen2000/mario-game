d83f5a54d91af735644d2a2b8df43bf2cc521084
vunguyen <vu.nh@kootoro.com>
2023-11-27 00:15:22 +0700

    add animationFrame to aniamation

--- 05-SceneManager/Animations.cpp
+++ 05-SceneManager/Animations.cpp
@@ -3,18 +3,57 @@
 
 CAnimations* CAnimations::__instance = NULL;
 
+
 CAnimations* CAnimations::GetInstance()
 {
 	if (__instance == NULL) __instance = new CAnimations();
 	return __instance;
 }
 
-void CAnimations::Add(int id, LPANIMATION ani)
+void CAnimation::Add(int spriteId, DWORD time)
 {
-	if (animations[id] != NULL)
-		DebugOut(L"[WARNING] Animation %d already exists\n", id);
+	int t = time;
+	if (time == 0) t = this->defaultTime;
 
-	animations[id] = ani;
+	LPSPRITE sprite = CSprites::GetInstance()->Get(spriteId);
+
+	if (sprite == NULL)
+	{
+		DebugOut(L"[ERROR] Sprite ID %d cannot be found!\n", spriteId);
+	}
+
+	LPANIMATION_FRAME frame = new CAnimationFrame(sprite, t);
+	frames.push_back(frame);
+}
+
+void CAnimation::Render(float x, float y, int alpha)
+{
+	DWORD now = GetTickCount();
+	if (currentFrame == -1)
+	{
+		currentFrame = 0;
+		lastFrameTime = now;
+	}
+	else
+	{
+		DWORD t = frames[currentFrame]->GetTime();
+		if (now - lastFrameTime > t)
+		{
+			currentFrame++;
+			lastFrameTime = now;
+			if (currentFrame == frames.size()) currentFrame = 0;
+		}
+	}
+
+	frames[currentFrame]->GetSprite()->Draw(x, y, alpha);
+}
+
+CAnimations* CAnimations::__instance = NULL;
+
+CAnimations* CAnimations::GetInstance()
+{
+	if (__instance == NULL) __instance = new CAnimations();
+	return __instance;
 }
 
 LPANIMATION CAnimations::Get(int id)
@@ -25,6 +64,21 @@ LPANIMATION CAnimations::Get(int id)
 	return ani;
 }
 
+void CAnimations::Add(int id, LPANIMATION ani)
+{
+	animations[id] = ani;
+}
+
+
+LPANIMATION CAnimations::Get(int id)
+{
+	LPANIMATION ani = animations[id];
+	if (ani == NULL)
+		DebugOut(L"[ERROR] Failed to find animation id: %d\n", id);
+	return ani;
+}
+
+
 void CAnimations::Clear()
 {
 	for (auto x : animations)
@@ -34,4 +88,29 @@ void CAnimations::Clear()
 	}
 
 	animations.clear();
+}
+
+CAnimationSets::CAnimationSets()
+{
+
+}
+
+CAnimationSets* CAnimationSets::GetInstance()
+{
+	if (__instance == NULL) __instance = new CAnimationSets();
+	return __instance;
+}
+
+LPANIMATION_SET CAnimationSets::Get(unsigned int id)
+{
+	LPANIMATION_SET ani_set = animation_sets[id];
+	if (ani_set == NULL)
+		DebugOut(L"[ERROR] Failed to find animation set id: %d\n", id);
+
+	return ani_set;
+}
+
+void CAnimationSets::Add(int id, LPANIMATION_SET ani_set)
+{
+	animation_sets[id] = ani_set;
 }
--- 05-SceneManager/Animations.h
+++ 05-SceneManager/Animations.h
@@ -1,6 +1,38 @@
 #pragma once
+#include <unordered_map>
+#include <Windows.h>
+#include <Windows.h>
+#include <d3dx9.h>
+#include "Sprites.h"
 
-#include "Animation.h"
+
+class CAnimationFrame
+{
+	LPSPRITE sprite;
+	DWORD time;
+
+public:
+	CAnimationFrame(LPSPRITE sprite, int time) { this->sprite = sprite; this->time = time; }
+	DWORD GetTime() { return time; }
+	LPSPRITE GetSprite() { return sprite; }
+};
+
+typedef CAnimationFrame* LPANIMATION_FRAME;
+
+class CAnimation
+{
+	DWORD lastFrameTime;
+	int currentFrame;
+	int defaultTime;
+	vector<LPANIMATION_FRAME> frames;
+public:
+	CAnimation(int defaultTime = 100) { this->defaultTime = defaultTime; lastFrameTime = -1; currentFrame = -1; }
+	void Add(int spriteId, DWORD time = 0);
+
+	void Render(float x, float y, int alpha = 255);
+};
+
+typedef CAnimation* LPANIMATION;
 
 class CAnimations
 {
@@ -9,9 +41,29 @@ class CAnimations
 	unordered_map<int, LPANIMATION> animations;
 
 public:
+
 	void Add(int id, LPANIMATION ani);
 	LPANIMATION Get(int id);
 	void Clear();
 
 	static CAnimations* GetInstance();
+};
+
+typedef vector<LPANIMATION> CAnimationSet;
+
+typedef CAnimationSet* LPANIMATION_SET;
+
+class CAnimationSets
+{
+	static CAnimationSets* __instance;
+
+	unordered_map<int, LPANIMATION_SET> animation_sets;
+
+public:
+	CAnimationSets();
+	void Add(int id, LPANIMATION_SET ani);
+	LPANIMATION_SET Get(unsigned int id);
+
+
+	static CAnimationSets* GetInstance();
 };
