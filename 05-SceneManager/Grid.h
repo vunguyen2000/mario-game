93fddb7852fc8cd0fdfe1566c98bf9b6dbfdde9c
vunguyen <vu.nh@kootoro.com>
2023-11-26 23:58:51 +0700

    add grid

---
+++ 05-SceneManager/Grid.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Cell.h"
+#include "Utils.h"
+#include "Game.h"
+#include "GameObject.h"
+
+#define GRID_SECTION_SETTINGS	1
+#define GRID_SECTION_OBJECTS	2
+#define MAX_GRID_LINE 1024
+
+class CGrid
+{
+	int numRow, numCol;
+	int cellWidth;
+	int  cellHeight;
+	Cell** cells;
+
+	void _ParseSection_SETTINGS(string line);
+	void _ParseSection_OBJECTS(string line);
+public:
+	CGrid() {}
+	CGrid(LPCWSTR filePath);
+	void GetObjects(vector<LPGAMEOBJECT>& listObject, int camX, int camY);
+	void Load(LPCWSTR filePath);
+	void Unload();
+};
