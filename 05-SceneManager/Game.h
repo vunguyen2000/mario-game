50dc130d9800e7b1b80e0ac061c47ca556e19c15
vunguyen <vu.nh@kootoro.com>
2023-11-27 09:34:52 +0700

    update game

--- 05-SceneManager/Game.h
+++ 05-SceneManager/Game.h
@@ -5,16 +5,16 @@
 #include <unordered_map>
 #include <string>
 #include <d3dx9.h>
-#include <dinput.h>
 #include "KeyEventHandler.h"
 #include <d3d9.h>
 
-
+#define DIRECTINPUT_VERSION 0x0800
+#include <dinput.h>
+#include "Scence.h"
 using namespace std;
 
 #define MAX_FRAME_RATE 100
 #define KEYBOARD_BUFFER_SIZE 1024
-#define DIRECTINPUT_VERSION 0x0800
 #define KEYBOARD_STATE_SIZE 256
 
 
@@ -27,58 +27,53 @@ class CGame
 	static CGame* __instance;
 	HWND hWnd;									// Window handle
 
-	int backBufferWidth = 0;					// Backbuffer width & height, will be set during Direct3D initialization
-	int backBufferHeight = 0;
-	LPDIRECT3D9 d3d = NULL;
-	ID3D10Device* pD3DDevice = NULL;
-	IDXGISwapChain* pSwapChain = NULL;
-	ID3D10RenderTargetView* pRenderTargetView = NULL;
-	ID3D10BlendState* pBlendStateAlpha = NULL;			// To store alpha blending state
+	LPDIRECT3D9 d3d = NULL;						// Direct3D handle
+	LPDIRECT3DDEVICE9 d3ddv = NULL;				// Direct3D device object
+
 	LPDIRECT3DSURFACE9 backBuffer = NULL;
-	LPD3DX10SPRITE spriteObject;						// Sprite handling object, BIG MYSTERY: it has to be in this place OR will lead to access violation in D3D11.dll ????
+	LPD3DXSPRITE spriteHandler = NULL;			// Sprite helper library to help us draw 2D image on the screen 
 
 	LPDIRECTINPUT8       di;		// The DirectInput object         
 	LPDIRECTINPUTDEVICE8 didv;		// The keyboard device 
 
-	BYTE  keyStates[KEYBOARD_STATE_SIZE];			// DirectInput keyboard state buffer 
+	BYTE  keyStates[256];			// DirectInput keyboard state buffer 
 	DIDEVICEOBJECTDATA keyEvents[KEYBOARD_BUFFER_SIZE];		// Buffered keyboard data
-	LPDIRECT3DDEVICE9 d3ddv = NULL;
-	LPD3DXSPRITE spriteHandler = NULL;
 
 	LPKEYEVENTHANDLER keyHandler;
 
 	float cam_x = 0.0f;
 	float cam_y = 0.0f;
+
+	int gameTime = 300;
+
 	int screen_width;
 	int screen_height;
-	int gameTime = 300;
-	HINSTANCE hInstance;
-
-	ID3D10SamplerState* pPointSamplerState;
 
 	unordered_map<int, LPSCENE> scenes;
 	int current_scene;
-	int next_scene = -1;
 
 	void _ParseSection_SETTINGS(string line);
 	void _ParseSection_SCENES(string line);
 
 public:
 	// Init DirectX, Sprite Handler
-	void Init(HWND hWnd);
-	void Draw(float x, float y, LPDIRECT3DTEXTURE9 texture, int left, int top, int right, int bottom, int alpha = 255);
-
-	LPDIRECT3DTEXTURE9 LoadTexture(LPCWSTR texturePath);
-
-	// Keyboard related functions 
-	void InitKeyboard();
-	int IsKeyDown(int KeyCode);
-	void ProcessKeyboard();
-	void SetKeyHandler(LPKEYEVENTHANDLER handler) { keyHandler = handler; }
-
 	float GetCamPosX() { return cam_x; };
 	float GetCamPosY() { return cam_y; };
 	int GetGameTime() { return gameTime; };
+	void InitKeyboard();
+	void SetKeyHandler(LPKEYEVENTHANDLER handler) { keyHandler = handler; }
+	void Init(HWND hWnd);
+	void Draw(float x, float y, LPDIRECT3DTEXTURE9 texture, int left, int top, int right, int bottom, int alpha = 255);
+
+	int IsKeyDown(int KeyCode);
+	void ProcessKeyboard();
+
+	void Load(LPCWSTR gameFile);
+	LPSCENE GetCurrentScene() { return scenes[current_scene]; }
+	void SwitchScene(int scene_id);
+
+	int GetScreenWidth() { return screen_width; }
+	int GetScreenHeight() { return screen_height; }
 
 	static void SweptAABB(
 		float ml,			// move left 
@@ -96,29 +91,12 @@ public:
 		float& ny);
 
 	LPDIRECT3DDEVICE9 GetDirect3DDevice() { return this->d3ddv; }
-	IDXGISwapChain* GetSwapChain() { return this->pSwapChain; }
-	ID3D10RenderTargetView* GetRenderTargetView() { return this->pRenderTargetView; }
 	LPDIRECT3DSURFACE9 GetBackBuffer() { return backBuffer; }
 	LPD3DXSPRITE GetSpriteHandler() { return this->spriteHandler; }
 
-	ID3D10BlendState* GetAlphaBlending() { return pBlendStateAlpha; };
-
-	int GetBackBufferWidth() { return backBufferWidth; }
-	int GetBackBufferHeight() { return backBufferHeight; }
+	void SetCamPos(float x, float y) { cam_x = x; cam_y = y; }
 
 	static CGame* GetInstance();
-
-	void SetPointSamplerState();
-
-	void SetCamPos(float x, float y) { cam_x = x; cam_y = y; }
-	void GetCamPos(float& x, float& y) { x = cam_x; y = cam_y; }
-
-	LPSCENE GetCurrentScene() { return scenes[current_scene]; }
-	void Load(LPCWSTR gameFile);
-	void SwitchScene(int scene_id);
-	void InitiateSwitchScene(int scene_id);
-
 	~CGame();
 };
-typedef CGame* LPGAME;
 
--- 05-SceneManager/SampleKeyEventHandler.h
+++ 05-SceneManager/SampleKeyEventHandler.h
@@ -1,7 +1,6 @@
 #pragma once
 
-#include "Scene.h"
-
+#include <Scence.h>
 class CSampleKeyHandler: public CSceneKeyHandler
 {
 public:
