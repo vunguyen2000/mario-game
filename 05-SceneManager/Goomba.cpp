ab4167f976d65215ec4447046666e79a46a1a3ef
vunguyen <vu.nh@kootoro.com>
2023-11-28 21:59:56 +0700

    add Goomba move

--- 05-SceneManager/Goomba.cpp
+++ 05-SceneManager/Goomba.cpp
@@ -40,6 +40,43 @@ void CGoomba::CalcPotentialCollisions(vector<LPGAMEOBJECT>* coObjects, vector<LP
 
 void CGoomba::Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects){
 
+	CGameObject::Update(dt);
+
+	vy += GOOMBA_GRAVITY * dt;
+
+	vector<LPCOLLISIONEVENT> coEvents;
+	vector<LPCOLLISIONEVENT> coEventsResult;
+
+	coEvents.clear();
+
+	if (state != GOOMBA_STATE_DIE_DOWN && state != GOOMBA_STATE_DIE)
+		CalcPotentialCollisions(coObjects, coEvents);
+
+	if (coEvents.size() == 0)
+	{
+		x += dx;
+		y += dy;
+	}
+	else
+	{
+		float min_tx, min_ty, nx = 0, ny;
+		float rdx = 0;
+		float rdy = 0;
+
+		FilterCollision(coEvents, coEventsResult, min_tx, min_ty, nx, ny, rdx, rdy);
+
+
+		x += min_tx * dx + nx * 0.4f;
+		y += min_ty * dy + ny * 0.4f;
+		if (ny != 0) vy = 0;
+	
+	}
+	for (UINT i = 0; i < coEvents.size(); i++) delete coEvents[i];
+	if (x < 0)
+	{
+		x = GOOMBA_START_X;
+		vx = -vx;
+	}
 }
 
 
