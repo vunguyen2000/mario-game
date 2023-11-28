d8c16e237fdc7a4cf959a793590aaa9716c8c6cf
vunguyen <vu.nh@kootoro.com>
2023-11-28 21:56:31 +0700

    add animation goomba

--- 05-SceneManager/Goomba.cpp
+++ 05-SceneManager/Goomba.cpp
@@ -8,14 +8,37 @@ CGoomba::CGoomba()
 
 void CGoomba::GetBoundingBox(float &left, float &top, float &right, float &bottom)
 {
+	left = x;
+	top = y;
+	right = x + GOOMBA_BBOX_WIDTH;
+
+	if (state == GOOMBA_STATE_DIE)
+		bottom = y + GOOMBA_BBOX_HEIGHT_DIE;
+	else
+		bottom = y + GOOMBA_BBOX_HEIGHT;
+	if (state == GOOMBA_STATE_DIE)
+	{
+		right = left = bottom = top = 0;
+	}
 }
 
 void CGoomba::CalcPotentialCollisions(vector<LPGAMEOBJECT>* coObjects, vector<LPCOLLISIONEVENT>& coEvents)
 {
-
+	for (UINT i = 0; i < coObjects->size(); i++) {
+		LPCOLLISIONEVENT e = SweptAABBEx(coObjects->at(i));
+		if (dynamic_cast<CGoomba*>(coObjects->at(i)))
+		{
+			continue;
+		}
+		if (e->t > 0 && e->t <= 1.0f)
+			coEvents.push_back(e);
+		else
+			delete e;
+	}
+	std::sort(coEvents.begin(), coEvents.end(), CCollisionEvent::compare);
 }
 
-void CGoomba::Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects)
+void CGoomba::Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects){
 
 }
 
@@ -32,4 +55,16 @@ void CGoomba::Render()
 
 void CGoomba::SetState(int state)
 {
+	CGameObject::SetState(state);
+	switch (state)
+	{
+	case GOOMBA_STATE_DIE:
+		vx = 0;
+		vy = -0.2f;
+		break;
+	case GOOMBA_STATE_WALKING:
+		vx = -GOOMBA_WALKING_SPEED;
+		break;
+
+	}
 }
