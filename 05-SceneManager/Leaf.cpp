50ff489c2e427d7c8956941e965848ad6d5a4203
vunguyen <vu.nh@kootoro.com>
2023-11-29 22:08:16 +0700

    add cleaf va ham chung

---
+++ 05-SceneManager/Leaf.cpp
@@ -0,0 +1,94 @@
+#include "Leaf.h"
+#include "Mario.h"
+#include "PlayScene.h"
+#include <algorithm>
+
+void CLeaf::CalcPotentialCollisions(vector<LPGAMEOBJECT>* coObjects, vector<LPCOLLISIONEVENT>& coEvents)
+{
+
+	for (UINT i = 0; i < coObjects->size(); i++)
+	{
+		LPCOLLISIONEVENT e = SweptAABBEx(coObjects->at(i));
+		if (!dynamic_cast<CMario*>(coObjects->at(i)))
+		{
+			continue;
+		}
+
+		if (e->t > 0 && e->t <= 1.0f)
+			coEvents.push_back(e);
+		else
+			delete e;
+	}
+
+	std::sort(coEvents.begin(), coEvents.end(), CCollisionEvent::compare);
+
+}
+
+void CLeaf::Render()
+{
+	animation_set->at(0)->Render(x, y);
+	//RenderBoundingBox();
+}
+
+void CLeaf::GetBoundingBox(float& l, float& t, float& r, float& b)
+{
+	l = x;
+	t = y;
+	r = x + LEAF_BBOX_WIDTH;
+	b = y + LEAF_BBOX_WIDTH;
+}
+void CLeaf::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	LPSCENE scene = CGame::GetInstance()->GetCurrentScene();
+	CMario* mario = ((CPlayScene*)scene)->GetPlayer();
+	CGameObject::Update(dt, coObjects);
+
+	vy += LEAF_GRAVITY * dt;
+
+	vector<LPCOLLISIONEVENT> coEvents;
+	vector<LPCOLLISIONEVENT> coEventsResult;
+
+	coEvents.clear();
+
+	CalcPotentialCollisions(coObjects, coEvents);
+
+	if (coEvents.size() == 0)
+	{
+		x += dx;
+		y += dy;
+	}
+	else
+	{
+		float min_tx, min_ty, nx = 0, ny;
+		float rdx = 0;
+		float rdy = 0;
+
+		FilterCollision(coEvents, coEventsResult, min_tx, min_ty, nx, ny, rdx, rdy);
+
+		x += min_tx * dx + nx * 0.4f;
+		y += min_ty * dy + ny * 0.4f;
+
+		vy = 0;
+
+		for (UINT i = 0; i < coEventsResult.size(); i++)
+		{
+			LPCOLLISIONEVENT e = coEventsResult[i];
+			if (dynamic_cast<CMario*>(e->obj))
+			{
+				if (mario->GetLevel() == MARIO_LEVEL_BIG || mario->GetLevel() == MARIO_LEVEL_SMALL)
+				{
+					mario->SetLevel(MARIO_LEVEL_FOX);
+					mario->y -= 10;
+				}
+				if (mario->GetLevel() == MARIO_LEVEL_FOX)
+				{
+					mario->y -= 10;
+					mario->ScoreUp();
+				}
+				isDisAppear = true;
+			}
+		}
+
+	}
+	for (UINT i = 0; i < coEvents.size(); i++) delete coEvents[i];
+}
---
+++ 05-SceneManager/Leaf.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "GameObject.h"
+#define LEAF_BBOX_WIDTH  16
+#define LEAF_BBOX_HEIGHT  14
+
+#define LEAF_ANI_RIGHT  0
+#define LEAF_ANI_LEFT  1
+
+#define LEAF_GRAVITY 0.00001f
+class CLeaf
+{
+	int status;
+
+public:
+	virtual void Render();
+	virtual void GetBoundingBox(float& l, float& t, float& r, float& b);
+	void CalcPotentialCollisions(vector<LPGAMEOBJECT>* coObjects, vector<LPCOLLISIONEVENT>& coEvents);
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
+};
+
