e94f046607e972749413450653390a607862a927
Vunguyen <vunguyenscorpio@gmail.com>
2024-01-18 01:17:18 +0700

    add Card

---
+++ 05-SceneManager/Card.cpp
@@ -0,0 +1,15 @@
+#include "Card.h"
+void CCard::Render()
+{
+}
+
+void CCard::GetBoundingBox(float& l, float& t, float& r, float& b)
+{
+
+}
+
+void CCard::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+
+}
+
---
+++ 05-SceneManager/Card.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "GameObject.h"
+
+
+class CCard : public CGameObject
+{
+public:
+	virtual void Render();
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
+	virtual void GetBoundingBox(float& l, float& t, float& r, float& b);
+};
+
