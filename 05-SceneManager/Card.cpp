457c770879cc6ce5e6fc8b306287393eb47137cf
Vunguyen <vunguyenscorpio@gmail.com>
2024-01-18 01:23:18 +0700

    star and mushroom card

--- 05-SceneManager/Card.cpp
+++ 05-SceneManager/Card.cpp
@@ -1,15 +1,45 @@
 #include "Card.h"
 void CCard::Render()
 {
+	switch (state)
+	{
+	case CARD_STATE_MUSHROOM:
+	{
+		animation_set->at(CARD_ANI_MUSHROOM)->Render(x, y);
+		break;
+	}
+	case CARD_STATE_STAR:
+	{
+		animation_set->at(CARD_ANI_STAR)->Render(x, y);
+		break;
+	}
+	default:
+		break;
+	}
 }
 
 void CCard::GetBoundingBox(float& l, float& t, float& r, float& b)
 {
+		l = x;
+		t = y;
+		r = x + CARD_BBOX_WIDTH;
+		b = y + CARD_BBOX_HEIGHT;
 
 }
 
 void CCard::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 {
-
+	if (GetTickCount() - timeChange >= 1000)
+	{
+		if (state < 3)
+		{
+			state++;
+		}
+		else
+		{
+			state = CARD_STATE_MUSHROOM;
+		}
+		timeChange = GetTickCount();
+	}
 }
 
--- 05-SceneManager/Card.h
+++ 05-SceneManager/Card.h
@@ -4,10 +4,16 @@
 
 #define CARD_BBOX_WIDTH  26
 #define CARD_BBOX_HEIGHT  26
+#define CARD_STATE_MUSHROOM  1
+#define CARD_STATE_STAR  2
+#define CARD_ANI_MUSHROOM  0
+#define CARD_ANI_STAR  1
 
 class CCard : public CGameObject
 {
 public:
+	DWORD timeChange = 0;
+	CCard() { timeChange = GetTickCount(); SetState(CARD_STATE_STAR); };
 	virtual void Render();
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	virtual void GetBoundingBox(float& l, float& t, float& r, float& b);
